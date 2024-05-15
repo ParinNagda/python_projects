5235259a8b082b50d94c57198cab86ce7b3ea6c1
Parin <parinnagda@gmail.com>
2024-05-16 04:47:15 +0530

    update image generation

--- main.py
+++ main.py
@@ -1,60 +1,25 @@
-import time
-import turtle
-from turtle import Turtle, Screen
-import random
-tim = Turtle()
-turtle.colormode(255)
+import colorgram
+import turtle as t
+import  random
+t.colormode(255)
+tim = t.Turtle()
+tim.penup()
+color_list = [(249, 248, 244), (250, 245, 248), (243, 250, 246), (236, 244, 250), (236, 224, 80), (197, 7, 71), (195, 164, 13), (201, 75, 15), (231, 54, 132), (110, 179, 216)]
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
 
-def random_color():
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b = random.randint(0,255)
-    color = (r, g, b)
-    return color
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
-tim.shape("turtle")
-tim.color("red")
-tim.fillcolor("red")
-# tim.forward(100)
-# tim.right(90)
-# tim.forward(100)
-# tim.right(90)
-# tim.forward(100)
-# tim.right(90)
-# tim.forward(100)
 
-# for _ in range(0, 15):
-#     tim.forward(10)
-#     tim.penup()
-#     tim.forward(10)
-#     tim.pendown()
-colors = ["Violet","Indigo","Blue", "Green", "Yellow", "Orange", "Red", "Black"]
-# i=0
-# for val in range(3,11):
-#     degree = 360
-#     angle = 360 / val
-#     tim.color(colors[i])
-#     for _ in range(val):
-#         tim.forward(100)
-#         tim.left(angle)
-#     i +=1
-
-# directions = [0, 90, 180, 270]
-# i = 0;
-# tim.pensize(15)
-# tim.speed("fastest")
-# while i < 200:
-#     tim.color(random_color())
-#     tim.forward(30)
-#     tim.setheading(random.choice(directions))
-#     i+=1
-for _ in range(0,100):
-    tim.speed("fastest")
-    tim.color(random_color())
-    tim.circle(100)
-    heading = tim.heading()
-    tim.setheading(heading + 10)
-    tim.circle(100)
-
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
