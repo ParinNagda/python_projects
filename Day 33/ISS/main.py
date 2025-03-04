d865f6fbdd3cf1b54a58a560ca1d4447a2764cbf
Parin Nagda <parinnagda@gmail.com>
2025-03-04 11:03:35 +0530

    Day 33

--- Day 33/ISS/main.py
+++ Day 33/ISS/main.py
@@ -1,10 +1,39 @@
+import time
+
 import requests
+import datetime as dt
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 19.2304168
+MY_LON = 73.0766709
 
-response.raise_for_status()
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
 
-data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-print(data)
+    data = response.json()
+    iss_longitude = data["iss_position"]["longitude"]
+    iss_latitude = data["iss_position"]["latitude"]
+
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LON-5 <= iss_longitude <= MY_LON+5:
+        return True
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LON,
+        "formatted": 0
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+
+    data = response.json()
+    sunrise = int(data['results']['sunrise'].split("T")[1].split(':')[0])
+    sunset = int(data['results']['sunset'].split("T")[1].split(':')[0])
+    time_now = dt.datetime.now().hour
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+while True:
+    if is_iss_overhead() and is_night():
+        print("ISS is above my head")
